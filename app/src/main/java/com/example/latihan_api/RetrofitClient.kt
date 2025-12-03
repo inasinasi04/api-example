a3b04530cd2e64e8f16dd22c3e8ff65a37cfbb8d
inasinasi04 <inasafifah04@gmail.com>
2025-12-03 14:09:46 +0700

    latihan-api

---
+++ app/src/main/java/com/example/latihan_api/RetrofitClient.kt
@@ -0,0 +1,18 @@
+package com.example.latihan_api
+
+import com.example.latihan_api.repository.CatatanRepository
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+
+object RetrofitClient {
+    private const val BASE_URL ="http://192.168.18.100:8000/api/"
+
+    val catatanRepository: CatatanRepository by lazy {
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(CatatanRepository::class.java)
+    }
+}
