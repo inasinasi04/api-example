e52fb574239fbb65d80b34ffcde7bd1f25601123
inasinasi04 <inasafifah04@gmail.com>
2025-12-11 10:32:06 +0700

    read data

--- app/src/main/java/com/example/latihan_api/RetrofitClient.kt
+++ app/src/main/java/com/example/latihan_api/RetrofitClient.kt
@@ -6,7 +6,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 
 object RetrofitClient {
-    private const val BASE_URL ="http://192.168.18.100:8000/api/"
+    private const val BASE_URL ="http:/192.168.12.132:8000/api/"
 
     val catatanRepository: CatatanRepository by lazy {
         Retrofit.Builder()
